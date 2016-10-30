5be3faef762ab8199bd56429fd0b7abc9843c218
nukosuke <nukosuke@shibuya.tech>
2016-10-31 02:02:48 +0900

    refactor url routes

--- entry/urls.py
+++ entry/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<entry_id>.+)$', views.show, name='show'),
+    url(r'^$', views.index, name='entry-index'),
+    url(r'^(?P<entry_id>\d+)/edit$', views.edit, name='entry-edit'),
+    url(r'^(?P<slug>.+)$', views.show, name='entry-show'),
 ]
