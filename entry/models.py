84b827c95241e9586f69685d67036748e298466e
nukosuke <nukosuke@shibuya.tech>
2016-10-30 21:26:21 +0900

    add entry model

--- entry/models.py
+++ entry/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Entry(models.Model):
+    slug         = models.SlugField()
+    title        = models.CharField(max_length=255)
+    body         = models.TextField()
+    created_at   = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(default=None)
