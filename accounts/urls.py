4499fbbd7ebfdd9a513ac372080b51bd3008aae4
nukosuke <nukosuke@shibuya.tech>
2016-10-31 19:09:47 +0900

    :running: add accouts route

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    url(r'^login/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'accounts/logout.html'}, name='logout'),
+]
