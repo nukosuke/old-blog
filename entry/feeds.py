6032f91e2be39c99736cf4f8c7db16175e7e5393
nukosuke <nukosuke@shibuya.tech>
2016-11-01 03:03:58 +0900

    add feeds package

---
+++ entry/feeds.py
@@ -0,0 +1,17 @@
+from django.contrib.syndication.views import Feed
+from django.urls import reverse
+from .models import Entry
+
+class RssFeed(Feed):
+    title = 'blog entry'
+    link = '/entries/'
+    description = 'blog entries'
+
+    def items(self):
+        return Entry.objects.order_by('-published_at')[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_link(self, item):
+        return reverse('entry-show', args=[item.slug])
