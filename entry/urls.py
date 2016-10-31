73e91be825d02310f071f6225aad99045163a5a5
nukosuke <nukosuke@shibuya.tech>
2016-10-31 17:46:59 +0900

    add routes

--- entry/urls.py
+++ entry/urls.py
@@ -2,7 +2,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='entry-index'),
-    url(r'^(?P<entry_id>\d+)/edit$', views.edit, name='entry-edit'),
+    # pages
+    url(r'^$', views.index, name='entry-index'), # entry list
+    url(r'^new/edit$', views.new_edit, name='entry-new-edit'), # new entry editor page
+    url(r'^(?P<entry_id>\d+)/edit$', views.edit, name='entry-edit'), # exists entry editor page
+
+    # post endpoint
+    url(r'^new$', views.new, name='entry-new'),
+    url(r'^(?P<entry_id>\d+)/update$', views.update, name='entry-update'), # update exists entry
+    url(r'^(?P<entry_id>\d+)/delete$', views.delete, name='entry-delete'), # delete entry
     url(r'^(?P<slug>.+)$', views.show, name='entry-show'),
 ]
