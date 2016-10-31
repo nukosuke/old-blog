f2448438da61757c0ff0515dab84a525a90afd61
nukosuke <nukosuke@shibuya.tech>
2016-10-31 18:38:16 +0900

    :jack_o_lantern: rename account -> accounts

--- blog/urls.py
+++ blog/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^accounts/', include('accounts.urls')),
     url(r'^entry/', include('entry.urls')),
     url(r'^admin/', admin.site.urls),
 ]
