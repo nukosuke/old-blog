fc6f9c059cc4b082eedfc912e2651a1520228f6d
nukosuke <nukosuke@shibuya.tech>
2016-11-18 02:43:13 +0900

    layout editor form

--- entry/forms.py
+++ entry/forms.py
@@ -1,7 +1,10 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from .models import Entry
 
 class EntryForm(ModelForm):
     class Meta:
         model = Entry
         fields = ['body',]
+        widgets = {
+            'body': Textarea(attrs={'class': 'editor-form'})
+        }
